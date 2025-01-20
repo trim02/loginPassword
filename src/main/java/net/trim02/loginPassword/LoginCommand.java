b0ae5abdce188dfbcad96503678ee8b37b249973
trim02 <me@trim02.net>
2025-01-20 09:50:06 +0100

    Removed comment out code

--- src/main/java/net/trim02/loginPassword/LoginCommand.java
+++ src/main/java/net/trim02/loginPassword/LoginCommand.java
@@ -43,18 +43,17 @@ public class LoginCommand implements SimpleCommand {
             source.sendMessage(Component.text(configVar.noPassword, NamedTextColor.RED));
             return;
         }
-//        source.sendMessage(Component.text("Hello World from a command!", NamedTextColor.AQUA));
         if (source instanceof Player player && args[0].equals(configVar.serverPassword)) {
             Optional<RegisteredServer> connectToServer = server.getServer(configVar.hubServer);
             player.createConnectionRequest(connectToServer.get()).connectWithIndication();
 
             if (!player.hasPermission("group." + configVar.bypassGroup) && (configVar.oneTimeLogin && configVar.pluginGrantsBypass) && configVar.bypassMethod.equals("group")) {
-//                System.out.println("---- Adding group ----");
+
                 lpApi.getUserManager().modifyUser(player.getUniqueId(), user -> {
                     user.data().add(Node.builder("group." + configVar.bypassGroup).build());
                 });
             } else if (!player.hasPermission(configVar.bypassNode) && (configVar.oneTimeLogin && configVar.pluginGrantsBypass) && configVar.bypassMethod.equals("user")) {
-//                System.out.println("---- Adding node ----");
+
                 lpApi.getUserManager().modifyUser(player.getUniqueId(), user -> {
                     user.data().add(Node.builder(configVar.bypassNode).build());
                 });
@@ -74,14 +73,9 @@ public class LoginCommand implements SimpleCommand {
     public boolean hasPermission(final Invocation invocation) {
         boolean sourcePermissionNode = invocation.source().hasPermission(configVar.bypassNode);
         boolean sourcePermissionGroup = invocation.source().hasPermission("group." + configVar.bypassGroup);
-//        System.out.println("User has group: " + invocation.source().hasPermission("group." + configVar.bypassGroup));
-//        System.out.println("User has node: " + invocation.source().hasPermission(configVar.bypassNode));
-//        System.out.println("Bypass method: " + configVar.bypassMethod);
-//        System.out.println("One Time Login: " + configVar.oneTimeLogin);
-//        System.out.println("disable command: " + configVar.disableLoginCommandOnBypass);
         if (configVar.loginCommandNegated) {
             if ((configVar.oneTimeLogin && configVar.disableLoginCommandOnBypass) && ((configVar.bypassMethod.equals("group") && sourcePermissionGroup) || (configVar.bypassMethod.equals("user") && sourcePermissionNode))) {
-//                System.out.println("This command should be disabled");
+
                 return false;
             } else {
                 return true;
@@ -90,7 +84,6 @@ public class LoginCommand implements SimpleCommand {
         } else {
             return invocation.source().hasPermission("loginpassword.login");
         }
-//        return (configVar.oneTimeLogin && configVar.disableLoginCommandOnBypass) && !(configVar.bypassMethod.equals("group") && sourcePermissionGroup) || !(configVar.bypassMethod.equals("user") && sourcePermissionNode);
 
 
     }
--- src/main/java/net/trim02/loginPassword/PlayerConnection.java
+++ src/main/java/net/trim02/loginPassword/PlayerConnection.java
@@ -35,13 +35,6 @@ public class PlayerConnection {
 
         if ((configVar.oneTimeLogin) && ((configVar.bypassMethod.equals("group") && player.hasPermission(configVar.bypassGroup)) || ((configVar.bypassMethod.equals("user") && player.hasPermission(configVar.bypassNode))))) {
             return;
-//            System.out.println("Player is in group");
-////            player.transferToHost(new InetSocketAddress("localhost",25566));
-//            Optional<RegisteredServer> connectToServer = server.getServer(configVar.hubServer);
-//            System.out.println("RegisteredServer Info: " + connectToServer.get().getServerInfo().getName());
-////            player.createConnectionRequest(connectToServer.get()).connectWithIndication();
-//            event.setInitialServer(connectToServer.get());
-
 
         } else {
             Optional<RegisteredServer> connectToServer = server.getServer(configVar.loginServer);
@@ -56,22 +49,12 @@ public class PlayerConnection {
         RegisteredServer connectedServer = event.getServer();
 
         if (connectedServer.getServerInfo().getName().equals(configVar.loginServer)) {
-//            System.out.println("--- First SCE event ---");
-//            System.out.println("Config server: " + configVar.loginServer);
-//            System.out.println("Player " + player.getUsername() + " has logged in to server: " + connectedServer.getServerInfo().getName());
-//            server.getScheduler().buildTask(plugin,() -> {
-//                System.out.println("This message is delayed by 10 seconds");
-//            }).delay(5L, TimeUnit.SECONDS).schedule();
             ScheduledTask task = server.getScheduler().buildTask(plugin, () -> {
 
-//                System.out.println("This message is delayed by 10 seconds: " + player);
                 Component kickMessage = ((configVar.kickMessage).equals("default")) ? Component.text("You were kicked for failing to provide the password within " + configVar.kickTimeout + " seconds") : Component.text(configVar.kickMessage);
                 player.disconnect(kickMessage);
             }).delay(configVar.kickTimeout, TimeUnit.SECONDS).schedule();
             hashScheduledPlayerTask.put(player.getUniqueId().hashCode(), String.valueOf(task.toString().hashCode()));
-//            System.out.println("Scheduled task: " + task.toString().hashCode());
-//            System.out.println("Hash Map everything: " + hashScheduledPlayerTask.size());
-//            System.out.println("--- First SCE END ---");
 
         }
 
@@ -87,30 +70,17 @@ public class PlayerConnection {
             return;
         }
         if (connectedServer.getServerInfo().getName().equals(configVar.hubServer) && Objects.equals(transferServer.get().getServerInfo().getName(), configVar.loginServer)) {
-//            System.out.println("------ Second SCE event ------");
-//            System.out.println("Player transfered from: " + transferServer.orElse(null).getServerInfo().getName());
-//            System.out.println("Player " + player.getUsername() + " has logged in to server: " + connectedServer.getServerInfo().getName());
-            ScheduledTask task = server.getScheduler().buildTask(plugin, () -> {
+            server.getScheduler().buildTask(plugin, () -> {
                 Collection<ScheduledTask> tasks = server.getScheduler().tasksByPlugin(plugin);
-//            System.out.println(tasks.stream().distinct(hashScheduledPlayerTask.get(player.getUniqueId().toString())));
-//            ScheduledTask quitTask = hashScheduledPlayerTask.get(player.getUniqueId().toString());
                 for (ScheduledTask cancelTask : tasks) {
-//                    System.out.println("Canceling tasks...");
-//                    int cancelTaskHashCode = cancelTask.toString().hashCode();
                     if (hashScheduledPlayerTask.containsKey(player.getUniqueId().hashCode())) {
-//                        System.out.println("Task: " + cancelTask.toString().hashCode());
 
-//                        System.out.println("1 Task status: " + cancelTask.status());
                         if (!cancelTask.status().toString().equals("FINISHED")) {
-//                            System.out.println("Player has quit, canceling task");
                             cancelTask.cancel();
                         }
-//                        System.out.println("2 Task status: " + cancelTask.status());
+
                     }
                 }
-//                System.out.println("All tasks: " + tasks);
-//                System.out.println("Hash map : " + hashScheduledPlayerTask.get(player.getUniqueId().hashCode()));
-//                System.out.println("--- Second SCE END ---");
             }).delay(1, TimeUnit.SECONDS).schedule();
         }
     }
