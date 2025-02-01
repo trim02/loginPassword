a6043c194f99e9f0410062ec9194f3336cd95529
trim02 <me@trim02.net>
2025-02-01 01:26:59 +0100

    Made LuckPerms an optional dependency for the plugin

--- src/main/java/net/trim02/loginPassword/LoginCommand.java
+++ src/main/java/net/trim02/loginPassword/LoginCommand.java
@@ -8,9 +8,6 @@ import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import net.luckperms.api.LuckPerms;
-import net.luckperms.api.LuckPermsProvider;
-import net.luckperms.api.node.Node;
 import net.trim02.loginPassword.loginPassword.configVar;
 import org.slf4j.Logger;
 
@@ -20,19 +17,12 @@ public class LoginCommand implements SimpleCommand {
     protected final ProxyServer server;
     protected final Logger logger;
 
-//    public static boolean luckPermsIsPresent;
 
     public LoginCommand(ProxyServer server, Logger logger) {
         this.server = server;
         this.logger = logger;
-//        if(pluginManager.getPlugin("LuckPerms").isPresent()) {
-//            lpApi = LuckPermsProvider.get();
-//            luckPermsIsPresent = true;
-//        }
+ 
     }
-//    public static boolean luckPermsPresent(){
-//        return luckPermsIsPresent;
-//    }
 
     @Override
     public void execute(Invocation invocation) {
@@ -44,34 +34,20 @@ public class LoginCommand implements SimpleCommand {
             source.sendMessage(Component.text(configVar.noPassword, NamedTextColor.RED));
             return;
         }
-         if (source instanceof Player player && args[0].equals(configVar.serverPassword)) {
-             Optional<RegisteredServer> connectToServer = server.getServer(configVar.hubServer);
-             player.createConnectionRequest(connectToServer.get()).connectWithIndication();
-             logger.info("Player {} has logged in", player.getUsername());
-         }
-        //     if (!player.hasPermission(configVar.bypassNode) && (configVar.oneTimeLogin && configVar.pluginGrantsBypass)) {
-        //         if (configVar.bypassMethod.equalsIgnoreCase("user")) {
-        //             lpApi.getUserManager().modifyUser(player.getUniqueId(), user -> {
-        //                 user.data().add(Node.builder(configVar.bypassNode).build());
-        //             });
-        //         } else if (configVar.bypassMethod.equalsIgnoreCase("group")) {
-        //             lpApi.getUserManager().modifyUser(player.getUniqueId(), user -> {
-        //                 user.data().add(Node.builder("group." + configVar.bypassGroup).build());
-        //             });
-        //         }
-        //     else {
-        //             logger.error("An error occurred while granting the user {} bypass permission. This shouldn't happen. Bother trim", player.getUsername());
-        //        source.sendMessage(Component.text("An error occurred. Please inform server staff.", NamedTextColor.RED));
-        //     }
-        //     }
+        if (source instanceof Player player && args[0].equals(configVar.serverPassword)) {
+            Optional<RegisteredServer> connectToServer = server.getServer(configVar.hubServer);
+            player.createConnectionRequest(connectToServer.get()).connectWithIndication();
 
-        // } 
-            else if (!args[0].equals(configVar.serverPassword)) {
+            // logger.info("Player {} has logged in", player.getUsername());
+
+        } else if (!args[0].equals(configVar.serverPassword)) {
             source.sendMessage(Component.text(configVar.wrongPassword, NamedTextColor.RED));
         } else if (source instanceof ConsoleCommandSource) {
             source.sendMessage(Component.text("This command can only be run by a player", NamedTextColor.RED));
         } else {
-            source.sendMessage(Component.text("An error occurred. This shouldn't happen. Bother trim", NamedTextColor.RED));
+            source.sendMessage(
+                    Component.text("An error occurred. This shouldn't happen. Bother trim", NamedTextColor.RED));
+                    logger.error("An error occurred. This shouldn't happen. Bother trim");
         }
 
     }
@@ -79,7 +55,6 @@ public class LoginCommand implements SimpleCommand {
     @Override
     public boolean hasPermission(final Invocation invocation) {
         boolean sourcePermissionNode = invocation.source().hasPermission(configVar.bypassNode);
-        // boolean sourcePermissionGroup = invocation.source().hasPermission("group." + configVar.bypassGroup);
 
         if (configVar.loginCommandNegated) {
             return !(configVar.oneTimeLogin && configVar.disableLoginCommandOnBypass && sourcePermissionNode);
