cc007738aa5893b78474c8ec7c6aad0ea1088330
trim02 <me@trim02.net>
2025-10-29 05:32:51 +0100

    Add integration against luckperms and viaversion to check if player has bypass perms and add them if configured and check if client has required version to show dialogs

--- src/main/java/net/trim02/loginPassword/paper/DialogLogin.java
+++ src/main/java/net/trim02/loginPassword/paper/DialogLogin.java
@@ -1,12 +1,9 @@
 package net.trim02.loginPassword.paper;
 
-import com.destroystokyo.paper.event.player.PlayerJumpEvent;
-import com.google.common.io.ByteArrayDataOutput;
-import com.google.common.io.ByteStreams;
-import io.papermc.paper.connection.PlayerCommonConnection;
+import com.destroystokyo.paper.event.player.PlayerConnectionCloseEvent;
+import com.viaversion.viaversion.api.Via;
+import com.viaversion.viaversion.api.ViaAPI;
 import io.papermc.paper.connection.PlayerConfigurationConnection;
-import io.papermc.paper.connection.PlayerConnection;
-import io.papermc.paper.connection.PlayerGameConnection;
 import io.papermc.paper.dialog.Dialog;
 import io.papermc.paper.dialog.DialogResponseView;
 import io.papermc.paper.event.connection.configuration.AsyncPlayerConnectionConfigureEvent;
@@ -16,15 +13,16 @@ import io.papermc.paper.registry.RegistryKey;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.Component;
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.node.Node;
+import net.trim02.loginPassword.Config.configVar;
+import net.trim02.loginPassword.common.BypassList;
 import net.trim02.loginPassword.loginPasswordPaper;
-import org.bukkit.Bukkit;
 import org.bukkit.Server;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.slf4j.Logger;
-import net.trim02.loginPassword.Config.configVar;
 
 import java.util.Map;
 import java.util.UUID;
@@ -35,78 +33,115 @@ import java.util.concurrent.TimeUnit;
 import static net.trim02.loginPassword.BuildConstants.DIALOG_NAMESPACE;
 
 public class DialogLogin implements Listener {
-    private loginPasswordPaper plugin;
-    private Server server;
+    private static ViaAPI viaAPI;
+    private static LuckPerms lpApi;
     private final Logger logger;
     private final Map<UUID, CompletableFuture<Boolean>> connectingPlayers = new ConcurrentHashMap<>();
+    private loginPasswordPaper plugin;
+    private Server server;
 
     public DialogLogin(loginPasswordPaper plugin, Server server, Logger logger) {
         this.plugin = plugin;
         this.server = server;
         this.logger = logger;
+
+        if (server.getPluginManager().getPlugin("LuckPerms") != null) {
+            lpApi = LuckPermsProvider.get();
+        } else {
+            lpApi = null;
+        }
+        if (server.getPluginManager().getPlugin("ViaVersion") != null) {
+            viaAPI = Via.getAPI();
+        } else {
+            viaAPI = null;
+        }
     }
 
-
-
-
-    @EventHandler
-    public void onPlayerJump(PlayerJumpEvent event) {
-        Player player = event.getPlayer();
-        logger.info("Sending player to hub2 via plugin message channel.");
-        ByteArrayDataOutput out = ByteStreams.newDataOutput();
-        out.writeUTF("Message");
-        out.writeUTF("hub2");
-        player.sendPluginMessage(plugin, loginPasswordPaper.PluginChannelFull, out.toByteArray());
-    }
 
     @EventHandler
     void onPlayerConfiguration(AsyncPlayerConnectionConfigureEvent event) {
-        Dialog dialog = RegistryAccess.registryAccess().getRegistry(RegistryKey.DIALOG).get(Key.key(DIALOG_NAMESPACE, "login_dialog"));
-        if (dialog == null){
-            logger.error("Failed to load dialog");
+        if (!configVar.pluginEnabled) {
+            return;
+        }
 
+
+        Dialog dialog = RegistryAccess.registryAccess().getRegistry(RegistryKey.DIALOG).get(Key.key(DIALOG_NAMESPACE, "login_dialog"));
+
+        if (dialog == null) {
+
+            event.getConnection().disconnect(Component.text("Internal server error"));
+            logger.error("Failed to load dialog for player {}", event.getConnection().getProfile().getName());
             return;
         }
 
         PlayerConfigurationConnection connection = event.getConnection();
         UUID playerUUID = connection.getProfile().getId();
-//        UUID testUUID = UUID.fromString("0c8a1f84-c036-447c-a946-fec2524dc9d6");
-//        server.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
-//            logger.info("Player name via sync: {}", connection.getProfile().getName());
-//            logger.info("CraftPlayer: {}", server.getOfflinePlayer(playerUUID));
-//        });
-//        logger.info("Test profile via async: {}", server.getOfflinePlayer(testUUID));
-//        logger.info("Player UUID: {}", playerUUID);
+
+
+
         if (playerUUID == null) {
-            logger.error("Player UUID is null");
+            event.getConnection().disconnect(Component.text("Internal server error"));
+            logger.error("Player {} UUID is null", connection.getProfile().getName());
             return;
         }
-//        logger.info("Player profile: {}", connection.getProfile());
-//        if (server.getPlayer(playerUUID) == null) {
-//            logger.info("Bypassing login for trim02");
-//            return;
-//        }
+        if (configVar.oneTimeLogin) {
+            if (lpApi != null) {
+                var user = lpApi.getUserManager().getUser(playerUUID);
+                if (user != null && user.getCachedData().getPermissionData().checkPermission(configVar.bypassNode).asBoolean()) {
+
+                    return;
+                }
+            }
+            if (BypassList.inBypassList(playerUUID)) {
+
+                return;
+            }
+        }
+
+        if (viaAPI != null) {
+            if (viaAPI.getPlayerVersion(playerUUID) < 771) {
+                logger.info("Player {} attempted to join using version: {}, which does not support the dialog api required to show the login prompt. They have been disconnected. They must update or be granted bypass permissions using LuckPerms or added to the bypass list using /loginpassword add {}", connection.getProfile().getName(), viaAPI.getPlayerProtocolVersion(playerUUID), playerUUID);
+                connection.disconnect(Component.text("Unsupported Minecraft version. Please use 1.21.6 or higher."));
+                return;
+            }
+        }
         CompletableFuture<Boolean> response = new CompletableFuture<>();
         response.completeOnTimeout(false, configVar.kickTimeout, TimeUnit.SECONDS);
         connectingPlayers.put(playerUUID, response);
 
         Audience audience = connection.getAudience();
-        logger.info("Response: {}", response);
         audience.showDialog(dialog);
-        logger.info("Displayed login dialog to player {}", connection.getProfile().getName());
+
 
         if (!response.join()) {
             audience.closeDialog();
-
             connection.disconnect(Component.text("Login failure"));
             logger.info("Player {} failed to log in", connection.getProfile().getName());
         }
-        logger.info("Player {} has logged in or timed out", connection.getProfile().getName());
+        if (response.isDone() && response.join() && (configVar.oneTimeLogin && configVar.pluginGrantsBypass)) {
+            if (lpApi != null) {
+                if (configVar.bypassMethod.equalsIgnoreCase("user")) {
+                    lpApi.getUserManager().modifyUser(playerUUID, user -> {
+                        user.data().add(Node.builder(configVar.bypassNode).build());
+                    });
+                } else if (configVar.bypassMethod.equalsIgnoreCase("group")) {
+                    lpApi.getUserManager().modifyUser(playerUUID, user -> {
+                        user.data().add(Node.builder("group." + configVar.bypassGroup).build());
+                    });
+                } else {
+                    logger.error("An error occurred while granting the user {} bypass permission.", connection.getProfile().getName());
+                }
+            } else {
+                BypassList.addBypassEntry(String.valueOf(playerUUID));
+            }
+            logger.info("Player {} successfully logged in", connection.getProfile().getName());
+        }
         connectingPlayers.remove(playerUUID);
 
 
-
     }
+
+
     @EventHandler
     void onHandleDialog(PlayerCustomClickEvent event) {
         if (!(event.getCommonConnection() instanceof PlayerConfigurationConnection configurationConnection)) {
@@ -115,19 +150,18 @@ public class DialogLogin implements Listener {
 
         UUID playerUUID = configurationConnection.getProfile().getId();
         if (playerUUID == null) {
-            logger.error("Player UUID is null");
+
+            configurationConnection.disconnect(Component.text("Internal server error"));
+            logger.error("Player {} UUID is null", configurationConnection.getProfile().getName());
             return;
         }
 
         Key dialogKey = event.getIdentifier();
-        logger.info("Player {} clicked dialog with key {}", configurationConnection.getProfile().getName(), dialogKey);
-
         DialogResponseView responseView = event.getDialogResponseView();
-        logger.info("Processing login dialog for player {}", configurationConnection.getProfile().getName());
-        logger.info("DialogResponseView: {}", responseView);
-        if(responseView == null) {
+
+        if (responseView == null) {
+            configurationConnection.disconnect(Component.text("Internal server error"));
             logger.error("DialogResponseView is null for player {}", playerUUID);
-            logger.warn("They will be disconnected");
             setConnectionResult(playerUUID, false);
             return;
         }
@@ -135,37 +169,30 @@ public class DialogLogin implements Listener {
             configurationConnection.getAudience().closeDialog();
             configurationConnection.disconnect(Component.text("Disconnected from server"));
             setConnectionResult(playerUUID, false);
-            logger.info("Player {} has exited login dialog", configurationConnection.getProfile().getName());
+
             return;
         }
-        if(!dialogKey.equals(Key.key(DIALOG_NAMESPACE, "submit_login"))) {
+        if (!dialogKey.equals(Key.key(DIALOG_NAMESPACE, "submit_login"))) {
+            configurationConnection.disconnect(Component.text("Internal server error"));
             setConnectionResult(playerUUID, false);
             return;
         }
         String passwordInput = responseView.getText("password_input");
         if (passwordInput.isEmpty()) {
-            logger.error("Password input is null for player {}", playerUUID);
-            logger.warn("They will be disconnected");
             configurationConnection.disconnect(Component.text(configVar.noPassword));
             setConnectionResult(playerUUID, false);
             return;
         }
         if (passwordInput.equals(configVar.serverPassword)) {
             setConnectionResult(playerUUID, true);
-            logger.info("Player {} has logged in", configurationConnection.getProfile().getName());
         } else {
             configurationConnection.getAudience().closeDialog();
             configurationConnection.disconnect(Component.text(configVar.wrongPassword));
             setConnectionResult(playerUUID, false);
-            logger.info("Player {} provided wrong password", configurationConnection.getProfile().getName());
         }
 
-
-
-
-
-
     }
+
 
     private void setConnectionResult(UUID playerUUID, boolean value) {
         CompletableFuture<Boolean> response = connectingPlayers.get(playerUUID);
@@ -174,5 +201,9 @@ public class DialogLogin implements Listener {
         }
     }
 
+    @EventHandler
+    void onPlayerConnectionClose(PlayerConnectionCloseEvent event){
+        connectingPlayers.remove(event.getPlayerUniqueId());
+    }
 
 }
